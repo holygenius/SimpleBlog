7741cd7106567734bc33fe39d0c3b6f1edc79ba6
erenyaman <erencyaman@gmail.com>
2025-11-21 17:24:06 +0300

    test: Temporarily remove base to debug CMS auth

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,4 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://holygenius.github.io', // Your full domain, or GitHub Pages domain
-  base: '/SimpleBlog', // Your repository name, if it's a project page
 });
